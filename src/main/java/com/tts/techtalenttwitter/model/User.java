3a6831bd7dcd01fb59068c81ef6e3f5865aea2a7
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2021-02-16 10:07:48 -0600

    added user service and implementation of

--- src/main/java/com/tts/techtalenttwitter/model/User.java
+++ src/main/java/com/tts/techtalenttwitter/model/User.java
@@ -1,15 +1,21 @@
 package com.tts.techtalenttwitter.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
+//import javax.management.relation.Role;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 @Data
 @Builder
@@ -26,14 +32,143 @@ public class User {
     @Email(message = "Please provide a valid email")
     @NotEmpty(message = "Please provide an email")
     private String email;
-    
+
+    @NotEmpty(message = "Please provide a username")
+    @Length(min = 3, message = "Your username must have at least 3 characters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
     private String userName;
+
+    @Length(min = 5, message = "Your password must have at least 5 characters")
+    @NotEmpty(message = "Please provide a password")
+//    @JsonProperty(access = Access.WRITE_ONLY)
     private String password;
+
+    @NotEmpty(message = "Please provide your first name")
     private String firstName;
+
+    @NotEmpty(message = "Please provide your last name")
     private String lastName;
+    /*
+    In our application, every user will have a value of 1 for active, indicating
+    that their account is enabled.
+     */
     private int active;
 
     @CreationTimestamp
     private Date createdAt;
+
+    /*
+    In order to use Spring security, we have to define roles for each
+    user. To keep things simple, every user in our application will
+    have the same role - "USER".
+     */
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_follower", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "follower_id"))
+    private List<User> followers;
+
+    @ManyToMany(mappedBy = "followers")
+    private List<User> following;
+
+    /*
+     // Use this code if your lombok is not working:
+    // public Long getId() {
+    // return id;
+    // }
+
+    // public String getEmail() {
+    // return email;
+    // }
+
+    // public void setEmail(String email) {
+    // this.email = email;
+    // }
+
+    // public String getUsername() {
+    // return username;
+    // }
+
+    // public void setUsername(String username) {
+    // this.username = username;
+    // }
+
+    // public String getPassword() {
+    // return password;
+    // }
+
+    // public void setPassword(String password) {
+    // this.password = password;
+    // }
+
+    // public String getFirstName() {
+    // return firstName;
+    // }
+
+    // public void setFirstName(String firstName) {
+    // this.firstName = firstName;
+    // }
+
+    // public String getLastName() {
+    // return lastName;
+    // }
+
+    // public void setLastName(String lastName) {
+    // this.lastName = lastName;
+    // }
+
+    // public int getActive() {
+    // return active;
+    // }
+
+    // public void setActive(int active) {
+    // this.active = active;
+    // }
+
+    // public Date getCreatedAt() {
+    // return createdAt;
+    // }
+
+    // public void setCreatedAt(Date createdAt) {
+    // this.createdAt = createdAt;
+    // }
+
+    // public Set<Role> getRoles() {
+    // return roles;
+    // }
+
+    // public void setRoles(Set<Role> roles) {
+    // this.roles = roles;
+    // }
+
+    // public List<User> getFollowers() {
+    // return followers;
+    // }
+
+    // public void setFollowers(List<User> followers) {
+    // this.followers = followers;
+    // }
+
+    // public List<User> getFollowing() {
+    // return following;
+    // }
+
+    // public void setFollowing(List<User> following) {
+    // this.following = following;
+    // }
+
+    // @Override
+    // public String toString() {
+    // return "User [active=" + active + ", createdAt=" + createdAt + ", email=" +
+    // email + ", firstName=" + firstName
+    // + ", followers=" + followers + ", following=" + following + ", id=" + id + ",
+    // lastName=" + lastName
+    // + ", password=" + password + ", roles=" + roles + ", username=" + username +
+    // "]";
+    // }
+     */
 
 }//end User class
---
+++ src/main/java/com/tts/techtalenttwitter/service/UserService.java
@@ -0,0 +1,19 @@
+package com.tts.techtalenttwitter.service;
+
+import com.tts.techtalenttwitter.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User findByUsername(String userName);
+
+    List<User> findAll();
+
+    void save(User user);
+
+    User saveNewUser(User user);
+
+    User getLoggedInUser();
+
+}//end UserService
